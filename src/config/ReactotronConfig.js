37257b97c583e04126d636d0a284a3fb400bfb99
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-12 15:46:28 -0300

    Configured store

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,12 @@
 import Reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+import reacotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure().connect();
+  const tron = Reactotron.configure()
+    .use(reactotronRedux())
+    .use(reacotronSaga())
+    .connect();
 
   tron.clear();
 
