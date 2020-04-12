90a379c5b9157fe16ac6a540ad071778c7356cfe
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-12 20:28:51 -0300

    Added Authentication

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,23 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token } = response.data;
+
+  yield put(signInSuccess(token));
+
+  history.push('/deliveries');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
