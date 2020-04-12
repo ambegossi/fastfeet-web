c2145e14ca82760c5b6ed6ad8ad8a19a39c4c8a3
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-12 14:43:52 -0300

    Added Unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="FastFeet" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <strong>SEU E-MAIL</strong>
-        <input type="email" placeholder="exemplo@email.com" />
+        <Input name="email" type="email" placeholder="exemplo@email.com" />
         <strong>SUA SENHA</strong>
-        <input type="password" placeholder="******" />
+        <Input name="password" type="password" placeholder="********" />
 
         <button type="submit">Entrar no sistema</button>
-      </form>
+      </Form>
     </>
   );
 }
