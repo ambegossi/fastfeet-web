9e343a11bed0d7700d2b618bd7feaafade36d4b4
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-13 14:18:22 -0300

    Added toasts

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -20,6 +21,7 @@ export function* signIn({ payload }) {
 
     history.push('/deliveries');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
