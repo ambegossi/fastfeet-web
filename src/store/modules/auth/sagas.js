5e00c3ede075681feaedbebc99c80e2bb1cf0fcb
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-13 14:30:18 -0300

    Added authenticated requests

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -17,6 +17,8 @@ export function* signIn({ payload }) {
 
     const { token, user } = response.data;
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push('/deliveries');
@@ -26,4 +28,17 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+]);
