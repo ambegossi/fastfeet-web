99320b60b26e695818fcd18f99ca109287a7340d
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-13 14:13:09 -0300

    Added authentication loading

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
@@ -16,6 +16,7 @@ const schema = Yup.object().shape({
 
 export default function SignIn() {
   const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
@@ -31,7 +32,7 @@ export default function SignIn() {
         <strong>SUA SENHA</strong>
         <Input name="password" type="password" placeholder="********" />
 
-        <button type="submit">Entrar no sistema</button>
+        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
       </Form>
     </>
   );
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -3,21 +3,25 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  yield put(signInSuccess(token, user));
+    yield put(signInSuccess(token, user));
 
-  history.push('/deliveries');
+    history.push('/deliveries');
+  } catch (err) {
+    yield put(signFailure());
+  }
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
