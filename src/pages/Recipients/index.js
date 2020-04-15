8bf5b5bd53b200e11604eb47ff9ec4bda7ab2448
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-15 14:09:27 -0300

    Added Recipients page

--- src/pages/Recipients/index.js
+++ src/pages/Recipients/index.js
@@ -1,7 +1,163 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { Form, Input } from '@rocketseat/unform';
+import {
+  MdCreate,
+  MdDeleteForever,
+  MdKeyboardArrowLeft,
+  MdKeyboardArrowRight,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import AddButton from '~/components/AddButton';
+import Empty from '~/components/Empty';
+
+import { TableWrapper, Table, ActionsMenu } from '~/pages/Deliverymen/styles';
+
+import {
+  Container,
+  SearchRow,
+  SearchIcon,
+  ActionsWrapper,
+  ActionsIcon,
+  PageNav,
+} from '~/pages/Deliveries/styles';
+
+import { IdTd, NameTd, AddressTd } from './styles';
+
+import api from '~/services/api';
+import history from '~/services/history';
 
 export default function Recipients() {
-  return <h1>Recipients</h1>;
+  const [recipients, setRecipients] = useState([]);
+  const [page, setPage] = useState(1);
+
+  async function loadRecipients(pageSelected, q) {
+    const response = await api.get('recipients', {
+      params: {
+        q,
+        page: pageSelected,
+      },
+    });
+
+    setRecipients(response.data);
+  }
+
+  useEffect(() => {
+    loadRecipients(page, null);
+  }, [page]);
+
+  function handleSearch({ searchInput }) {
+    const q = searchInput;
+    loadRecipients(null, q);
+  }
+
+  function increment() {
+    setPage(page + 1);
+  }
+
+  function decrement() {
+    setPage(page - 1);
+  }
+
+  function toggleMenu(recipientId) {
+    const formattedRecipients = recipients.map((recipient) => {
+      if (recipient.id === recipientId) {
+        recipient.showActionsMenu = !recipient.showActionsMenu;
+      } else {
+        recipient.showActionsMenu = false;
+      }
+      return recipient;
+    });
+
+    setRecipients(formattedRecipients);
+  }
+
+  function handleEdit(recipient) {
+    if (recipient.showActionsMenu) {
+      history.push({
+        pathname: '/recipients/edit',
+        state: { recipient },
+      });
+    }
+  }
+
+  async function handleDelete(id) {
+    const confirmation = window.confirm(
+      'Tem certeza que deseja apagar este destinatário?'
+    );
+
+    if (confirmation === true) {
+      try {
+        await api.delete(`recipients/${id}`);
+        toast.success('Destinatário deletado com sucesso!');
+        loadRecipients(page, null);
+      } catch (err) {
+        toast.error('Não foi possível deletar o destinatário do sistema');
+      }
+    }
+  }
+
+  return (
+    <Container>
+      <h1>Gerenciador de destinatários</h1>
+      <SearchRow>
+        <Form onSubmit={handleSearch}>
+          <button type="submit">
+            <SearchIcon size={19} color="#999" />
+          </button>
+          <Input name="searchInput" placeholder="Buscar por destinatários" />
+        </Form>
+        <AddButton onClick={() => history.push('/recipients/register')} />
+      </SearchRow>
+
+      {recipients.length > 0 ? (
+        <TableWrapper>
+          <Table>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Endereço</th>
+              <th>Ações</th>
+            </tr>
+
+            {recipients.map((recipient) => (
+              <tr>
+                <IdTd>{`#${recipient.id}`}</IdTd>
+                <NameTd>{recipient.name}</NameTd>
+                <AddressTd>{`${recipient.street}, ${recipient.street_number}, ${recipient.city} - ${recipient.state}`}</AddressTd>
+                <ActionsWrapper>
+                  <ActionsIcon
+                    size={30}
+                    onClick={() => toggleMenu(recipient.id)}
+                  />
+                  <ActionsMenu showActionsMenu={recipient.showActionsMenu}>
+                    <li onClick={() => handleEdit(recipient)}>
+                      <MdCreate color="#4D85EE" />
+                      <span>Editar</span>
+                    </li>
+                    <li onClick={() => handleDelete(recipient.id)}>
+                      <MdDeleteForever color="DE3B3B" />
+                      <span>Excluir</span>
+                    </li>
+                  </ActionsMenu>
+                </ActionsWrapper>
+              </tr>
+            ))}
+          </Table>
+        </TableWrapper>
+      ) : (
+        <Empty />
+      )}
+
+      <PageNav>
+        {page > 1 ? (
+          <MdKeyboardArrowLeft size={30} onClick={decrement} />
+        ) : (
+          <MdKeyboardArrowLeft size={30} color="#999" />
+        )}
+        <span>{page}</span>
+        <MdKeyboardArrowRight size={30} onClick={increment} />
+      </PageNav>
+    </Container>
+  );
 }
---
+++ src/pages/Recipients/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const IdTd = styled.td`
+  width: 50px;
+`;
+
+export const NameTd = styled.td`
+  width: 150px;
+  margin-left: 150px;
+`;
+
+export const AddressTd = styled.td`
+  width: 250px;
+  margin-left: 73px;
+`;
