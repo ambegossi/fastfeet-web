08bc95b061f350f323eff2025ac25a9d28f218b7
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-15 12:44:49 -0300

    Added DeliverymenRegister page

--- src/pages/Deliverymen/DeliverymenRegister/index.js
+++ src/pages/Deliverymen/DeliverymenRegister/index.js
@@ -1,7 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MdPhoto } from 'react-icons/md';
+import { toast } from 'react-toastify';
 
-// import { Container } from './styles';
+import UpdateRow from '~/components/UpdateRow';
+
+import {
+  Container,
+  FormContainer,
+  LineInput,
+  AvatarInput,
+  EmptyAvatar,
+} from './styles';
+
+import api from '~/services/api';
+import history from '~/services/history';
 
 export default function DeliverymenRegister() {
-  return <div />;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [file, setFile] = useState(null);
+  const [preview, setPreview] = useState(null);
+
+  async function handleAvatarChange(e) {
+    const data = new FormData();
+
+    data.append('file', e.target.files[0]);
+
+    const response = await api.post('files', data);
+
+    const { id, url } = response.data;
+
+    setFile(id);
+    setPreview(url);
+  }
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  const newData = {
+    avatar_id: file,
+    name,
+    email,
+  };
+
+  async function createDeliveryman(data) {
+    try {
+      if (data.avatar_id) {
+        await api.post('deliverymens', {
+          name: data.name,
+          email: data.email,
+          avatar_id: data.avatar_id,
+        });
+      } else {
+        await api.post('deliverymens', {
+          name: data.name,
+          email: data.email,
+        });
+      }
+
+      toast.success('Entregador cadastrado com sucesso!');
+      history.push('/deliverymen');
+    } catch (err) {
+      toast.error('Não foi possível cadastrar o entregador');
+      console.tron.log(err);
+    }
+  }
+
+  return (
+    <Container>
+      <UpdateRow
+        title="Cadastro de entregadores"
+        saveFunction={createDeliveryman}
+        data={newData}
+      />
+
+      <FormContainer>
+        <AvatarInput>
+          <label htmlFor="avatar">
+            {preview ? (
+              <img src={preview} alt={name} />
+            ) : (
+              <EmptyAvatar>
+                <MdPhoto size={50} color="#DDD" />
+                <span>Adicionar foto</span>
+              </EmptyAvatar>
+            )}
+            <input
+              type="file"
+              accept="image/*"
+              id="avatar"
+              onChange={handleAvatarChange}
+            />
+          </label>
+        </AvatarInput>
+        <LineInput>
+          <span>Nome</span>
+          <input
+            type="text"
+            placeholder="Nome do entregador aqui"
+            onChange={handleNameChange}
+          />
+        </LineInput>
+        <LineInput>
+          <span>Email</span>
+          <input
+            type="email"
+            placeholder="Email do entregador aqui"
+            onChange={handleEmailChange}
+          />
+        </LineInput>
+      </FormContainer>
+    </Container>
+  );
 }
---
+++ src/pages/Deliverymen/DeliverymenRegister/styles.js
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 900px;
+  margin: 40px auto;
+  align-self: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FormContainer = styled.div`
+  margin-top: 30px;
+  width: 100%;
+  background-color: #fff;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+
+  input {
+    height: 45px;
+    border: 1px solid #dddddd;
+    border-radius: 4px;
+    padding: 0 15px;
+    color: #444444;
+    font-size: 16px;
+  }
+
+  span {
+    font-weight: bold;
+    color: #444444;
+    margin-bottom: 10px;
+  }
+`;
+
+export const AvatarInput = styled.div`
+  margin: 20px;
+  align-self: center;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  border: 1px dashed #ddd;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  transition: opacity 0.2s;
+
+  label {
+    height: 100%;
+    width: 100%;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+
+  &:hover {
+    background: #f5f5f5;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+  }
+
+  input {
+    display: none;
+  }
+`;
+
+export const LineInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 30px 25px 30px;
+`;
+
+export const EmptyAvatar = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    color: #ddd;
+    margin-top: 10px;
+    font-weight: bold;
+  }
+`;
